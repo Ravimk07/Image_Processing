791aa2ceb2a8df91d28552f0b1c9b2d8fd66e366
Ravi Kamble <ravikamble07@gmail.com>
2018-09-25 23:50:57 +0530

    Add files via upload

---
+++ Sample_exp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 10:29:36 2018
+
+@author: Samiksha Pachade
+"""
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
